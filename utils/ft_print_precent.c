c0e2b6f912324b36d8437bd2dbba1d32a4759e1f
student <student@iMac-student.local>
2020-05-22 09:30:20 +0300

    f*mule

--- utils/ft_print_precent.c
+++ utils/ft_print_precent.c
@@ -20,10 +20,7 @@ int		ft_print_precent(t_lst list_flags)
 	if (list_flags.minus)
 	{
 		ft_putchar_fd('%', 1);
-		if (list_flags.zero)
-			count += ft_add_width('0', list_flags.width - 1);
-		else
-			count += ft_add_width(' ', list_flags.width - 1);
+		count += ft_add_width(' ', list_flags.width - 1);
 	}
 	else if (!list_flags.minus)
 	{
